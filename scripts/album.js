7cb09456714358b9797f4943a27b68399263eae2
Jenny <jennylhanlon@gmail.com>
2015-10-09 15:05:56 -0700

    Album View no song list

--- scripts/album.js
+++ scripts/album.js
@@ -7,10 +7,10 @@ var albumPicasso = {
   year: '1881',
   albumArtUrl: 'assets/images/album_covers/01.png',
   songs: [
-    {name:'Blue', length: '4:26'}
-    {name:'Green', length: '3:14'}
-    {name:'Red', length: '5:01'}
-    {name:'Pink', length: '3:21'}
+    {name:'Blue', length: '4:26'},
+    {name:'Green', length: '3:14'},
+    {name:'Red', length: '5:01'},
+    {name:'Pink', length: '3:21'},
     {name:'Magenta', length: '2:15'}
   ]
 };
@@ -25,10 +25,10 @@ var albumMarconi = {
   year: '1909',
   albumArtUrl: 'assets/images/album_covers/20.png',
   songs: [
-    {name:'Hello, Operator', length: '1:01'}
-    {name:'Ring, ring, ring', length: '5:01'}
-    {name:'Fits in your pocket', length: '3:21'}
-    {name:'Can you hear me now?', length: '3:14'}
+    {name:'Hello, Operator', length: '1:01'},
+    {name:'Ring, ring, ring', length: '5:01'},
+    {name:'Fits in your pocket', length: '3:21'},
+    {name:'Can you hear me now?', length: '3:14'},
     {name:'Wrong phone number', length: '2:15'}
   ]
 };
@@ -36,11 +36,11 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
 
   var template = 
-      '<tr class = "album-view-song-item">'
-     +' <td class = "song-item-number">' + songNumber + '</td>'
-     +' <td class = "song-item-title">' + songName + '</td>' 
-     +' <td class = "song-item-duration">' + songLength + '</td>'
-     +' </tr>'
+       '<tr class = "album-view-song-item">'
+     + ' <td class = "song-item-number">' + songNumber + '</td>'
+     + ' <td class = "song-item-title">' + songName + '</td>' 
+     + ' <td class = "song-item-duration">' + songLength + '</td>'
+     + ' </tr>'
      ;
 
   return template;
@@ -61,7 +61,7 @@ var setCurrentAlbum = function(album) {
   albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
   albumImage.setAttribute('src', album.albumArtUrl);
 
-  albumSongList.innerHTML = '';
+  albumSongList.innerHTML = ' ';
 
   for (i = 0; i < album.songs.length; i++) {
     albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
